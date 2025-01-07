1ce58a6a2be437a3daf523cb2796f9b88c6bea4c
Prabh ‪‪ <singh20prabh@gmail.com>
2025-01-07 15:02:19 +0530

    plans section completed

---
+++ src/components/plans/Plans.jsx
@@ -0,0 +1,46 @@
+import "./plans.css";
+import { plansData } from "../../data/plansData";
+import whiteTick from "/whiteTick.png";
+import rightArrow from "/rightArrow.png";
+
+function Plans() {
+  return (
+    <div className="plans-container">
+      <div className="programs-header" style={{ gap: "2rem" }}>
+        <span className="stroke-text">Ready to Start</span>
+        <span>Your Journey</span>
+        <span className="stroke-text">now withus</span>
+      </div>
+
+      {/* PLANS CARDS */}
+      <div className="plans">
+        {plansData.map((plan, i) => (
+          <div key={i} className="plan">
+            <img src={plan.icon} alt={plan.name} className="plan-icon" />
+            <span className="plan-name">{plan.name}</span>
+            <span className="plan-price">₹ {plan.price}</span>
+
+            <div className="features">
+              {plan.features.map((feature, i) => (
+                <div key={i} className="feature">
+                  <img src={whiteTick} alt="tick image" />
+                  <span>{feature}</span>
+                </div>
+              ))}
+            </div>
+
+            <div>
+              <span className="benefits">
+                See more benefits <img src={rightArrow} alt="right arrow" />
+              </span>
+            </div>
+            <button className="btn">Join now</button>
+          </div>
+        ))}
+      </div>
+      {/* PLANS CARDS */}
+    </div>
+  );
+}
+
+export default Plans;

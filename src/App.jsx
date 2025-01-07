b9fabaa649a751f1bc60afe179834c474d62b1ae
Prabh ‪‪ <singh20prabh@gmail.com>
2025-01-07 21:10:39 +0530

    testimonials section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero/Hero";
 import Plans from "./components/plans/Plans";
 import Programs from "./components/programs/Programs";
 import Reasons from "./components/reasons/Reasons";
+import Testimonials from "./components/testimonials/Testimonials";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Programs />
       <Reasons />
       <Plans />
+      <Testimonials />
     </div>
   );
 }
--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,25 +1,23 @@
-import image1 from "../assets/t-image1.png";
-import image2 from "../assets/t-image2.jpg";
-import image3 from "../assets/t-image3.jpg";
-
 export const testimonialsData = [
   {
-    image: image1,
+    image: "/t-image1.png",
     review:
       "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
+    name: "MATHEW HENDRICKSON",
+    status: "ENTREPRENEUR",
   },
   {
-    image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
-    status: 'COACH'
+    image: "/t-image2.jpg",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ",
+    name: "JOHN KEVIN",
+    status: "COACH",
   },
   {
-    image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
-    name: 'FRANKLIN',
-    status: "CUSTOMER"
-  }
+    image: "/t-image3.jpg",
+    review:
+      " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem",
+    name: "FRANKLIN",
+    status: "CUSTOMER",
+  },
 ];
